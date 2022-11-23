68f12c5098e653e3d7389e0e4150848faa5c6139
AyushiPohekar <ayushi2021.29@gmail.com>
2022-11-24 00:38:16 +0530

    gethotels chnaged

--- controllers/HotelController.js
+++ controllers/HotelController.js
@@ -43,15 +43,18 @@ export const getHotelById=async(req,res,next)=>{
    }
 }
 
-export const getHotels=async(req,res,next)=>{
+export const getHotels = async (req, res, next) => {
+    const { min, max, ...others } = req.query;
     try {
-        const Hotels=await Hotel.find();
-    
-        res.status(200).json(Hotels)
-    } catch (error) {
-        res.status(500).json(error)
+      const hotels = await Hotel.find({
+        ...others,
+        cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+      }).limit(req.query.limit);
+      res.status(200).json(hotels);
+    } catch (err) {
+      next(err);
     }
-}
+  };
 
 export const countByCity = async (req, res, next) => {
     const cities = req.query.cities.split(",");
